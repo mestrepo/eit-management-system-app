16b59a584233f981f8003ecfd302d1eb8e4df038
todun <todun@users.noreply.github.com>
2018-09-16 14:39:20 +0000

    refactored by removing commented out code

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -1,20 +1,14 @@
-import { Template } from 'meteor/templating';
+import {Template} from 'meteor/templating';
 
-import { Records } from '../api/records.js';
+import {Records} from '../api/records.js';
 
 import './record.js';
 import './body.html';
 
 Template.body.helpers({
     records() {
-        // const instance = Template.instance();
-        // if (instance.state.get('deleteselectedrecord')) {
-        //     // If hide completed is checked, filter tasks
-        //     return Records.find({ checked: { $ne: true } }, { sort: { createdAt: -1 } });
-        // }
-        // Otherwise, return all of the tasks
         // Show newest tasks at the top
-        return Records.find({}, { sort: { createdAt: -1 } });
+        return Records.find({}, {sort: {createdAt: -1}});
     },
 });
 
@@ -47,7 +41,4 @@ Template.body.events({
         target.gender.value = '';
         target.dob.value = '';
     },
-    // 'clicked .delete-selected-record button'(event, instance) {
-    //     instance.state.set('deleteselectedrecord', event.target.checked);
-    // },
 });
--- imports/ui/record.js
+++ imports/ui/record.js
@@ -14,12 +14,4 @@ Template.record.events({
     'click .delete'() {
         Records.remove(this._id);
     },
-    // 'click .delete-selected-record button'() {
-    //     // instance.state.set('deleteCompleted', event.target.checked);
-    //     Records.remove(this._id);
-    //     // Set the checked property to the opposite of its current value
-    //     // Records.remove(this._id, {
-    //     //     $set: { checked: ! this.checked },
-    //     // });
-    // },
 });
