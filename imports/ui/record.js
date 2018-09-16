093b97c51591571f1ef96c00e556f9d985268252
todun <todun@users.noreply.github.com>
2018-09-16 16:33:11 +0000

    Define helper to check ownership

--- imports/ui/record.js
+++ imports/ui/record.js
@@ -5,6 +5,12 @@ import { Records } from '../api/records.js';
 
 import './record.html';
 
+Template.record.helpers({
+    isOwner() {
+        return this.owner === Meteor.userId();
+    },
+});
+
 Template.record.events({
     'click .toggle-checked'() {
         Meteor.call('records.setChecked', this._id, !this.checked);
