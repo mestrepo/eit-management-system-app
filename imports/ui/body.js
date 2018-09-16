c1377c173df86833fc7ebd6d81006e1d46c567f4
todun <todun@users.noreply.github.com>
2018-09-16 02:52:43 +0000

    Add event handler for form submit

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -9,3 +9,29 @@ Template.body.helpers({
         return Records.find({});
     },
 });
+
+Template.body.events({
+    'submit .new-record'(event) {
+        // Prevent default browser form submit
+        event.preventDefault();
+
+        // Get value from form element
+        const target = event.target;
+        const firstname = target.text.value;
+        const surname = target.text.value;
+        const gender = target.text.value;
+        const dob = target.text.value;
+
+        // Insert a task into the collection
+        Records.insert({
+            firstname,
+            surname,
+            gender,
+            dob,
+            createdAt: new Date(), // current time
+        });
+
+        // Clear form
+        target.text.value = '';
+    },
+});
