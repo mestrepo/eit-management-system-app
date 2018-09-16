a7fd250705b460c9e9dbccb4e54f55f3082915b2
todun <todun@users.noreply.github.com>
2018-09-16 15:54:28 +0000

    Define some methods

--- imports/api/records.js
+++ imports/api/records.js
@@ -1,3 +1,37 @@
+import { Meteor } from 'meteor/meteor';
 import { Mongo } from 'meteor/mongo';
+import { check } from 'meteor/check';
 
 export const Records = new Mongo.Collection('records');
+
+Meteor.methods({
+    'records.insert'(text) {
+        check(text, String);
+
+        // Make sure the user is logged in before inserting a record
+        if (! Meteor.userId()) {
+            throw new Meteor.Error('not-authorized');
+        }
+
+        Records.insert({
+            firstname,
+            surname,
+            gender,
+            dob,
+            createdAt: new Date(), // current time
+            owner: Meteor.userId(),
+            username: Meteor.user().username,
+        });
+    },
+    'records.remove'(recordId) {
+        check(recordId, String);
+
+        Records.remove(recordId);
+    },
+    'records.setChecked'(recordId, setChecked) {
+        check(recordId, String);
+        check(setChecked, Boolean);
+
+        Records.update(recordId, { $set: { checked: setChecked } });
+    },
+});
