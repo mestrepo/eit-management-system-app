f6d310981b372ee95b1f4eed94f5dbab8e34d814
todun <todun@users.noreply.github.com>
2018-09-16 02:17:12 +0000

    Import view/UI JS

--- client/main.js
+++ client/main.js
@@ -1,12 +1,1 @@
-import { Template } from 'meteor/templating';
-
-import './main.html';
-
-if (Meteor.isClient) {
-    console.log("I'm a client");
-    Template.mytemplate.myFirstTemplateHelper = function () {
-        return "my first template";
-    }
-}
-
-if(Meteor.isServer) console.log("I'm a server");
+import '../imports/ui/body.js';
